bc05f3fac43ea066d438d9ea253054c2b8f3978c
daytnclp98 <106809590+daytnclp98@users.noreply.github.com>
2023-01-12 11:21:45 +0100

    Ajout liste

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -7,13 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import com.example.android.navigation.databinding.FragmentTitleBinding
+import java.util.NavigableMap
 
 
 /**
  * A simple [Fragment] subclass.
- * Use the [TitleFragment.newInstance] factory method to
- * create an instance of this fragment.
+
  */
 class TitleFragment : Fragment() {
 
@@ -21,6 +22,10 @@ class TitleFragment : Fragment() {
         savedInstanceState: Bundle?): View? {
         val binding: FragmentTitleBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_title, container, false)
+        
+        binding.playButton.setOnClickListener{ view : View->
+            Navigation.findNavController(view).navigate(R.id.action_titleFragment_to_gameFragment)
+        }
         return binding.root
     }
 
