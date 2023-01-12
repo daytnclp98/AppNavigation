af821c5a5c80d25a2a3ed6fcbce9efb6adb2d7f7
daytnclp98 <106809590+daytnclp98@users.noreply.github.com>
2023-01-12 11:40:46 +0100

    Titre fragement

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentTitleBinding
 import java.util.NavigableMap
 
@@ -22,10 +23,9 @@ class TitleFragment : Fragment() {
         savedInstanceState: Bundle?): View? {
         val binding: FragmentTitleBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_title, container, false)
-        
-        binding.playButton.setOnClickListener{ view : View->
-            Navigation.findNavController(view).navigate(R.id.action_titleFragment_to_gameFragment)
-        }
+        binding.playButton.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
+        )
         return binding.root
     }
 
